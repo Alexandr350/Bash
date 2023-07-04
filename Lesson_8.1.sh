044b3c144bb9787f891b9538ead971f12aed5c3f
Sanches <sanches251193@yandex.ru>
2023-07-04 10:39:01 +0300

    attempt2

--- Lesson_8.1.sh
+++ Lesson_8.1.sh
@@ -6,5 +6,4 @@ for((j=$i; j >= 200; j--))
         curl https://www.kartinki24.ru/uploads/gallery/main/195/kartinki24_ru_mountains_$j.jpg -L > attachments/Photo$j;
 done
 
-rm ./attachments/Photo*
-rm -rf ./attachments
+
---
+++ Lesson_8.2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm ./attachments/Photo*
+rm -rf ./attachments
